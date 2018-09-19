a89df4ac94fdbb29b76bbbb2a36b5729a3a2a1a6
Gtosta96 <gabrieltosta3@gmail.com>
2018-09-18 23:52:04 -0300

    configured basic architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    SharedModule
+  ],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      id: [''],
+    });
+  }
+
+  onSubmit(values) {
+    this.router.navigate(['/home', values.id]);
+  }
+}
